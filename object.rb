55cf0b66dfea40cb92a77eb241337570f3952dbb
Makoto Tanabe <xekid78@outlook.com>
2018-02-26 14:59:25 +0900

    Add files via upload

---
+++ object.rb
@@ -0,0 +1,13 @@
+text = "123"
+p text.to_i
+p text.length
+p text.class        # クラス名を調べる
+p "123".class
+
+players = "勇者,戦士,魔法使い,忍者"
+array = players.split(",")
+p array
+p array.length
+
+array2 = Array.new(3)
+p array2
